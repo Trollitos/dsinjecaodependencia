af37a821dc35f43de5dd832e424f35cbe8a38161
Vinicius Neri <vinicius.neri@outlook.com>
2025-01-06 18:48:56 -0300

    Criação do projeto spring

---
+++ src/main/java/com/trollitosdev/dsinjecaodependencia/DsinjecaodependenciaApplication.java
@@ -0,0 +1,13 @@
+package com.trollitosdev.dsinjecaodependencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsinjecaodependenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsinjecaodependenciaApplication.class, args);
+	}
+
+}
